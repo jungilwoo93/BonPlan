fe812dd45173323f8a95287b854ba6e5320c15bc
jungilwoo93 <panpan9311@gmail.com>
2018-12-29 22:24:23 +0100

    ajouter floating Button

--- app/src/main/java/com/example/panpa/bonplan/CalendarActivity.java
+++ app/src/main/java/com/example/panpa/bonplan/CalendarActivity.java
@@ -1,13 +1,16 @@
 package com.example.panpa.bonplan;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
-
+    private String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +21,18 @@ public class CalendarActivity extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = dayOfMonth + "/" + month + "/" + year;
+                date = dayOfMonth + "/" + (month+1) + "/" + year;
                 dateChoosed.setText(date);
                 calendarText.setText("You choose : ");
+            }
+        });
+        Button choose = findViewById(R.id.chooseDateButton);
+        choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(CalendarActivity.this, MainActivity.class);
+                intent.putExtra("date",date);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/panpa/bonplan/MainActivity.java
+++ app/src/main/java/com/example/panpa/bonplan/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.panpa.bonplan;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -19,11 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         Calendar cal=Calendar.getInstance();
-        int date = cal.get(Calendar.DAY_OF_MONTH);
+        int dateOfMonth = cal.get(Calendar.DAY_OF_MONTH);
         String monthname=(String)android.text.format.DateFormat.format("MMMM", new Date());
         int year = cal.get(Calendar.YEAR);
-        toolbar.setTitle(date + " " + monthname+" "+year);
+        toolbar.setTitle(dateOfMonth + " " + monthname+" "+year);
         setSupportActionBar(toolbar);
+
+        Intent intentFromCalendar = getIntent();
+        if(intentFromCalendar.getStringExtra("date")!=null){
+            String date = intentFromCalendar.getStringExtra("date");
+            toolbar.setTitle(date);
+        }
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,NoteEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
         //toolbar.setNavigationIcon(R.mipmap.calendarlogo);
         //Menu menu = findViewById(R..menu);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
