986de294f74eec0a8825b69bdbd35724d55bd575
jungilwoo93 <panpan9311@gmail.com>
2018-12-31 20:28:31 +0100

    ajouter les foncions de note et notes

--- app/src/main/java/com/example/panpa/bonplan/Plan/Note.java
+++ app/src/main/java/com/example/panpa/bonplan/Plan/Note.java
@@ -4,6 +4,73 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Note implements Parcelable {
+    private String title;
+    private String date;
+    private String place;
+    private String descrip;
+
+    public Note(String title,String date, String place, String descrip){
+        this.title=title;
+        this.date=date;
+        this.place=place;
+        this.descrip=descrip;
+    }
+
+    protected Note(Parcel in) {
+        this.title=in.readString();
+        this.date=in.readString();
+        this.place=in.readString();
+        this.descrip=in.readString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+
+
+
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
     @Override
     public int describeContents() {
         return 0;
@@ -11,6 +78,10 @@ public class Note implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.date);
+        dest.writeString(this.place);
+        dest.writeString(this.descrip);
 
     }
 }
--- app/src/main/java/com/example/panpa/bonplan/Plan/Notes.java
+++ app/src/main/java/com/example/panpa/bonplan/Plan/Notes.java
@@ -1,5 +1,37 @@
 package com.example.panpa.bonplan.Plan;
 
-public class Notes {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Notes {
+    private List<Note> notes;
+    public Notes(){
+
+    }
+
+    public List<Note> getNotes(){
+        if(notes == null){
+            notes = new ArrayList<Note>();
+        }
+        return this.notes;
+    }
+
+    public void add(Note note){
+        getNotes().add(note);
+    }
+
+    public void update(Note from , Note to){
+        if(getNotes().contains(from)){
+            int index = getNotes().indexOf(from);
+            getNotes().remove(from);
+            getNotes().set(index,to);
+        }
+
+    }
+
+    public void delete(Note note){
+        if(getNotes().contains(note)){
+            getNotes().remove(note);
+        }
+    }
 }
