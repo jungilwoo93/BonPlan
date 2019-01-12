d9b2d299e802302110c513a87c120e6b0097c500
jungilwoo93 <panpan9311@gmail.com>
2019-01-12 18:53:35 +0100

    add button back

--- app/src/main/java/com/example/panpa/bonplan/Activities/CalendarActivity.java
+++ app/src/main/java/com/example/panpa/bonplan/Activities/CalendarActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.panpa.bonplan.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
     private String date;
@@ -17,13 +24,22 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbarCalendar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);//设置toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 给左上角图标的左边加上一个返回的图标 。
+
         CalendarView calendar = findViewById(R.id.calendarView);
         final TextView dateChoosed = findViewById(R.id.dateText);
         final TextView calendarText = findViewById(R.id.chooseDateText);
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                date = dayOfMonth + "/" + (month+1) + "/" + year;
+                Calendar cal=Calendar.getInstance();
+                Date d = new Date(year,month,dayOfMonth);
+                SimpleDateFormat month_date = new SimpleDateFormat("MMMM");
+                String month_name = month_date.format(d);
+                date = dayOfMonth + " " + month_name + " " + year;
                 dateChoosed.setText(date);
                 calendarText.setText("You choose : ");
             }
@@ -37,5 +53,15 @@ public class CalendarActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent= new Intent(CalendarActivity.this, MainActivity.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/panpa/bonplan/Activities/NoteEditActivity.java
+++ app/src/main/java/com/example/panpa/bonplan/Activities/NoteEditActivity.java
@@ -1,9 +1,17 @@
 package com.example.panpa.bonplan.Activities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.panpa.bonplan.R;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class NoteEditActivity extends AppCompatActivity {
 
@@ -11,6 +19,19 @@ public class NoteEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_edit);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbarEdit);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);//设置toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 给左上角图标的左边加上一个返回的图标 。
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                Intent intent= new Intent(NoteEditActivity.this, MainActivity.class);
+                startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
